228a21d523815098759e9a7f4715d5fca5a4f536
Jhon Gesell Villanueva Portella <unmsm182@gmail.com>
2019-01-06 19:10:05 -0500

    linenew

--- matematicas01.py
+++ matematicas01.py
@@ -6,10 +6,12 @@ Created on Sun Jan  6 17:53:49 2019
 @author: jhongesell
 """
 
+# FUNCIONES
+
 import numpy as np
 
 def funcion(x):
-    return np.sqrt(x +2) # ( x + 2 ) ** 0.5
+    return np.sqrt((x**2) + (6*x) +5) # ( x + 2 ) ** 0.5
 x = np.array([-1 , 0, 3, 5, 10, 15])
 y = funcion(x)
 print(y)
@@ -38,12 +40,32 @@ def move_spines():
     
     return ax
 
-x = np.linspace(-2, 6, num=30)
+x = np.linspace(-2, 10, num=15)
 
 ax = move_spines()
 ax.grid()
 ax.plot(x, funcion(x))
-plt.title(r"Grafico de $f(x)=\sqrt{x +2}$")
+plt.title(r"Grafico de $f(x)=\sqrt{(x**2) + (6*x) +5}$")
+plt.ylabel('f(x)')
+plt.xlabel('x')
+plt.show()
+
+# LIMITES
+
+def f(x):
+    return x**2 -x + 2
+x = np.array([1, 1.5, 1.9, 1.95, 1.99, 2.001, 2.04, 2.005, 2.2, 2.5, 3])
+y=f(x)
+tabla = pd.DataFrame(list(zip(x,y)), columns=['X', 'f(x)'])
+print(tabla)
+
+x = np.linspace(-2, 4, num=30)
+ax = move_spines()
+ax.grid()
+ax.plot(x, f(x))
+ax.scatter(2, 4, label="limite cuando x tiende a 2", color='r')
+plt.legend()
+plt.title(r"Grafico de $f(x)=x^2 - x + 2$")
 plt.ylabel('f(x)')
 plt.xlabel('x')
 plt.show()
